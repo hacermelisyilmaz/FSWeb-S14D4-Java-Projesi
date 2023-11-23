207b3ae24aa26a5a9bb469a95cea13635cd02fcb
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-23 13:30:24 +0300

    ProductForSale class created w/ its variables, constructor & getters

---
+++ src/ProductForSale.java
@@ -0,0 +1,24 @@
+public abstract class ProductForSale {
+    private String type;
+    private double price;
+    private String description;
+
+    public ProductForSale(String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
