f57aa89a31c24f2b58e45784c7fbca661d5328f8
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-23 14:13:02 +0300

    Werewolf class created w/ constructor

---
+++ src/com/workintech/monster/Werewolf.java
@@ -0,0 +1,7 @@
+package com.workintech.monster;
+
+public class Werewolf extends Monster{
+    public Werewolf(String name, int hitPoints, double damage) {
+        super(name, hitPoints, damage);
+    }
+}
