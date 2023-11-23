d49a047411164caf6940e6cde9ae1577dff3654d
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-23 14:24:56 +0300

    Methods overridden in Werewolf class

--- src/com/workintech/monster/Werewolf.java
+++ src/com/workintech/monster/Werewolf.java
@@ -4,4 +4,14 @@ public class Werewolf extends Monster{
     public Werewolf(String name, int hitPoints, double damage) {
         super(name, hitPoints, damage);
     }
+
+    @Override
+    public double bleed() {
+        return 0.25 * getDamage();
+    }
+
+    @Override
+    public double poison() {
+        return 0.3 * getDamage();
+    }
 }
