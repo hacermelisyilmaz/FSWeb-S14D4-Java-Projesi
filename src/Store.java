b523062def3567c9f9a39a06c9732cd386f400f1
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-23 14:05:14 +0300

    ProductForSale[] array is created and passed to listProducts as arg

--- src/Store.java
+++ src/Store.java
@@ -1,7 +1,11 @@
 public class Store {
     public static void main(String[] args) {
-
-        listProducts();
+        ProductForSale products[] = new ProductForSale[]{
+                new Chocolate("dark", 10, "60% Dutch cocoa", 100),
+                new Bread("whole wheat", 6, "entirely milled from whole wheat grains", false),
+                new Coke("zero", 2, "zero sugar", 330)
+        };
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
